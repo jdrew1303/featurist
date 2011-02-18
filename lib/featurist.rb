1bf16bd58f108f73b3f4848e7b29c81cf718490a
Jon Archer <jon@rollinsville.org>
2011-02-17 22:23:29 -0700

    Modularized a bit

--- lib/featurist.rb
+++ lib/featurist.rb
@@ -1,79 +1,29 @@
-
 require 'featurist/options'
 require 'featurist/specification'
+include Featurist # TODO: figure out proper organization/naming/means to include stuff
+
+opts = Options.new
+opts.welcome
+
+# Create a spec
+spec = Featurist::Specification.new
+
+# Manually add any sections you want
+spec.add_or_update_section Featurist::SpecSection.new 1, "Introduction", "The intro...", spec.root
+
+# Dynamically generated stuff from "/Features"
+build_spec opts.dir, spec.root
+
+# Process the featurist.config
+# TODO: see above :)
+
+# Another manual add at the end
+spec.add_or_update_section Featurist::SpecSection.new 100, "Sign off", "Sign it in...PMED.", spec.root
+
+# Output our spec
+spec.diagnostics
+#@spec.print_spec
 
 
 
-class Featurist
-  def initialize
-    @opts = Options.new
-    @opts.welcome
 
-    @spec = Specification.new
-
-    # Other basic setup of spec
-    #@spec.cover_page ...
-    #@spec.toc = true
-    #@spec.page_numbering = true
-  end
-
-  def run
-    # Manually add any sections you want
-    @spec.add_or_update_section SpecSection.new(1, "Introduction", "The intro...", @spec.root)
-
-    # Dynamically generated stuff from "/Features"
-    build_spec @opts.dir, @spec.root
-
-    # Update section descriptions from /Features/**/*.sections
-
-
-    # Another manual add at the end
-    @spec.add_or_update_section SpecSection.new(1000, "Sign off", "Sign it in...PMED.", @spec.root)
-
-    @spec.diagnostics
-    #@spec.print_spec
-  end
-
-  def build_spec dir, section
-    Dir[ dir + '/*' ].each do |entry|
-      if File::directory?(entry)
-        # we need a SpecSection here to represent this
-        sub_section = SpecSection.new section.max_section_id + 1, entry.rpartition(/\//).last, entry, section
-        section.add_subsection sub_section
-        build_spec entry, sub_section
-      elsif entry.end_with? '.feature'
-        add_feature entry, section
-      end
-    end
-  end
-
-  def add_feature feature_file, section
-    File::open feature_file, 'r' do |feature|
-      feature_id = nil
-      feature_title = feature_narrative = line = ""
-      begin
-        next if line.length == 0
-        break if line.match 'Scenario|Background' # Scenario, Scenario Outline or Background = we're done
-        line.gsub!("\xEF\xBB\xBF", '') #strip off the stupid BOM-UTF8 marker that VS (sometimes) seems to slip in
-        line = line.split('#')[0] # chop off comments
-        if line.include? '@id'
-          feature_id = line.slice(/@id_\d+/).slice(/\d+/).to_i 
-        elsif line.include? 'Feature:'
-          feature_title = line.gsub('Feature:', '').lstrip
-        else
-          feature_narrative << line.lstrip
-        end
-      end while line = feature.gets
-      feature_id = section.max_section_id + 1 if feature_id.nil?
-      section.add_subsection SpecSection.new( feature_id, feature_title, feature_narrative, section)
-    end
-  end
-
-end
-
-
-
-# TODO: Massive amounts of instrumentation!!!!!!!
-
-featurist = Featurist.new
-featurist.run
--- lib/featurist/specification.rb
+++ lib/featurist/specification.rb
@@ -1,94 +1,138 @@
 require 'prawn'
 
-class SpecSection
-  attr_reader :req_id, :sub_sections, :parent_section
-  attr_accessor :title, :narrative
+module Featurist
+  class SpecSection
+    attr_reader :req_id, :sub_sections, :parent_section
+    attr_accessor :title, :narrative
 
-  def initialize req_id, title, narrative, parent_section
-    @req_id = req_id
-    @title = title
-    @narrative = narrative
-    @parent_section = parent_section
-    @sub_sections = {}
-  end
-
-  def add_subsection child
-    @sub_sections[child.req_id] = child
-  end
-
-  def ordered_sections
-    retval = []
-    @sub_sections.keys.sort.each do |key|
-      retval << @sub_sections[key]
+    def initialize req_id, title, narrative, parent_section
+      @req_id = req_id
+      @title = title
+      @narrative = narrative
+      @parent_section = parent_section
+      @sub_sections = {}
     end
-    retval
-  end
 
-  def max_section_id
-    ordered_sections.size
-  end
+    def add_subsection child
+      @sub_sections[child.req_id] = child
+    end
 
-  def fully_qualified_id
-    fqid = @req_id.to_s
-    parent = @parent_section
-    begin
-      fqid = parent.req_id.to_s + '.' + fqid unless parent.req_id == 0
-      parent = parent.parent_section
-    end while not parent.nil?
-    fqid
-  end
-end
+    def ordered_sections
+      retval = []
+      @sub_sections.keys.sort.each do |key|
+        retval << @sub_sections[key]
+      end
+      retval
+    end
 
-class Specification
-  attr_accessor :root
+    def max_section_id
+      ordered_sections.size
+    end
 
-  def initialize
-    @root = SpecSection.new 0, "Root", "Root", nil # magic root node -- constant?
-  end
-
-  def add_or_update_section section
-    if @root.sub_sections.has_key? section.req_id
-      # update the title, narrative -- leave children intact
-      @root.sub_sections[section.req_id].title = section.title
-      @root.sub_sections[section.req_id].narrative = section.narrative
-    else
-      @root.sub_sections[section.req_id] = section
+    def fully_qualified_id
+      fqid = @req_id.to_s
+      parent = @parent_section
+      begin
+        fqid = parent.req_id.to_s + '.' + fqid unless parent.req_id == 0
+        parent = parent.parent_section
+      end while not parent.nil?
+      fqid
     end
   end
 
-  def diagnostics
-    unwrap @root, 0
-  end
+  class Specification
+    attr_accessor :root
 
-  def unwrap node, indent
-    indent.times { print "  " }
-#    puts "#{node.req_id}.  #{node.title}" unless indent == 0 #ignore root
-    puts "#{node.fully_qualified_id}.  #{node.title}" unless indent == 0 #ignore root
-
-    node.ordered_sections.each do |sub_node|
-      unwrap sub_node, indent + 1
+    def initialize
+      @root = SpecSection.new 0, "Root", "Root", nil # magic root node -- constant?
     end
-  end
 
-  def print_spec
-    Prawn::Document.generate('spec.pdf') do |pdf|
-      # put the top level contents in order by key
-      #ordered = []
-      #@root.ordered_sections.keys.sort.each { |key| ordered << @contents[key] }
-      print_section @root.ordered_sections, pdf
+    def add_or_update_section section
+      if @root.sub_sections.has_key? section.req_id
+        # update the title, narrative -- leave children intact
+        @root.sub_sections[section.req_id].title = section.title
+        @root.sub_sections[section.req_id].narrative = section.narrative
+      else
+        @root.sub_sections[section.req_id] = section
+      end
+    end
+
+    def diagnostics
+      unwrap @root, 0
+    end
+
+    def unwrap node, indent
+      indent.times { print "  " }
+  #    puts "#{node.req_id}.  #{node.title}" unless indent == 0 #ignore root
+      puts "#{node.fully_qualified_id}.  #{node.title}" unless indent == 0 #ignore root
+
+      node.ordered_sections.each do |sub_node|
+        unwrap sub_node, indent + 1
+      end
+    end
+
+    def print_spec
+      Prawn::Document.generate('spec.pdf') do |pdf|
+        # put the top level contents in order by key
+        #ordered = []
+        #@root.ordered_sections.keys.sort.each { |key| ordered << @contents[key] }
+        print_section @root.ordered_sections, pdf
+      end
+    end
+
+    def print_section section, pdf
+      section.each do |r|
+        #pdf.text '<b>' << r.id.to_s << '. ' << r.title << '</b>', :inline_format => true
+        #pdf.text r.narrative
+        pdf.text "\n" # should be able to configure feature separation behavior
+
+        # deal with the children
+        #if not r.sub_sections.nil?
+        #  print_section r.sub_sections, pdf
+        #end
       end
     end
   end
 
-  def print_section section, pdf
-    section.each do |r|
-      #pdf.text '<b>' << r.id.to_s << '. ' << r.title << '</b>', :inline_format => true
-      #pdf.text r.narrative
-      pdf.text "\n" # should be able to configure feature separation behavior
+  #
+  # TODO: Where do the below methods really belong?
+  #       Currently Featurist::methods.
+  #
 
-      # deal with the children
-      #if not r.sub_sections.nil?
-      #  print_section r.sub_sections, pdf
-      #end
+  def build_spec dir, section
+    Dir[ dir + '/*' ].each do |entry|
+      if File::directory?(entry)
+        # we need a SpecSection here to represent this
+        sub_section = SpecSection.new section.max_section_id + 1, entry.rpartition(/\//).last, entry, section
+        section.add_subsection sub_section
+        build_spec entry, sub_section
+      elsif entry.end_with? '.feature'
+        add_feature entry, section
+      end
+    end
   end
+
+  def add_feature feature_file, section
+    File::open feature_file, 'r' do |feature|
+      feature_id = nil
+      feature_title = feature_narrative = line = ""
+      begin
+        next if line.length == 0
+        break if line.match 'Scenario|Background' # Scenario, Scenario Outline or Background = we're done
+        line.gsub!("\xEF\xBB\xBF", '') #strip off the stupid BOM-UTF8 marker that VS (sometimes) seems to slip in
+        line = line.split('#')[0] # chop off comments
+        if line.include? '@id'
+          feature_id = line.slice(/@id_\d+/).slice(/\d+/).to_i
+        elsif line.include? 'Feature:'
+          feature_title = line.gsub('Feature:', '').lstrip
+        else
+          feature_narrative << line.lstrip
+        end
+      end while line = feature.gets
+      feature_id = section.max_section_id + 1 if feature_id.nil?
+      section.add_subsection SpecSection.new( feature_id, feature_title, feature_narrative, section)
+    end
+  end
+
 end
+
